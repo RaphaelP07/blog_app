15d218306208c0214398f6c0d3de1f01149b92c1
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-07-06 11:41:23 +0800

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :articles
   devise_for :users
-  root to: "categories#index"
-  resources :categories
+  root to: "sessions#new"
+  resources :categories do
+    resources :articles do
+      resources :comments
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
